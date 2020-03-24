32bea1b8a8fc9a47212e75ed585f646a4fa9decc
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2020-03-23 19:23:07 -0600

    9) Conociendo Async/await

---
+++ src/async/index.js
@@ -0,0 +1,29 @@
+const doSomethingAsync = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('Do Something Async');
+		}, 3000);
+	});
+};
+
+const doSomething = async () => {
+	const something = await doSomethingAsync();
+	console.log(something);
+};
+
+console.log('Before');
+doSomething();
+console.log('After');
+
+const anotherFunction = async () => {
+	try {
+		const something = await doSomethingAsync();
+		console.log(something);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+console.log('Before1');
+anotherFunction();
+console.log('After1');
