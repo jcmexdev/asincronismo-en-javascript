1a15e62460b1df37425be4fae41f2f7cbd5b3b63
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2020-03-23 19:38:01 -0600

    10) Resolver problema con Async/Await

---
+++ src/async/challenge.js
@@ -0,0 +1,16 @@
+const fetchData = require('../utils/fetchData');
+let API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFunction = async () => {
+	try {
+		const data = await fetchData(API);
+		const character = await fetchData(`${API}${data.results[0].id}`);
+		const origin = await fetchData(character.origin.url);
+		console.log(data.info.count);
+		console.log(character.name);
+		console.log(origin.dimension);
+	} catch (error) {}
+};
+console.log('Before');
+anotherFunction();
+console.log('After');
